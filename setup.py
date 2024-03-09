0a2a784d8b8bf90e17f17deb7d00d2d8c3b7567b
OptimusCoderr <chukwuebukaanulunko@gmail.com>
2024-03-09 11:35:50 +0100

    Initial commit

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@
 from setuptools import find_packages,setup
 
 setup(
-    name='mcqgenerator',
+    name='detect_glaucoma',
     version='0.0.1',
     author='OptimusCoderr',
     author_email='chukwuebukaanulunko@gmail.com',
-    install_requires=["keras_cv", "keras","tensorflow","opencv-python", "pandas","numpy","tqdm", "joblib", "matplotlib","streamlit"],
+    install_requires=["keras_cv","keras","tensorflow","opencv-python", "pandas","numpy","matplotlib","streamlit","Pillow"],
     packages=find_packages()
 )
